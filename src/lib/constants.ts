9cc80a4571d3ff33013bd05a0533529f6e1bec98
Odilon Ferreira <odilon@atacana.com>
2024-06-01 14:08:18 -0300

    feat: create a const to keep sizes metrics

---
+++ src/lib/constants.ts
@@ -0,0 +1,22 @@
+export const KimonoSizes = {
+  1: {
+    height: [0, 165],
+    weight: [0, 60],
+  },
+  2: {
+    height: [166, 175],
+    weight: [61, 70],
+  },
+  3: {
+    height: [176, 180],
+    weight: [71, 80],
+  },
+  4: {
+    height: [181, 190],
+    weight: [81, 90],
+  },
+  5: {
+    height: [191, 215],
+    weight: [91, 110],
+  },
+};
